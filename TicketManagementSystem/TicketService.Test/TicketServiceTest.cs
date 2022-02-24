11ecf54416dd19c14ece902e488f2f0cb8f1c4eb
Jackw <Jackw@DESKTOP-BSI9TIM>
2022-02-24 09:27:33 +0000

    Add in check for ticket creation to ensure if i send data through its properly assigned.

--- TicketManagementSystem/TicketService.Test/TicketServiceTest.cs
+++ TicketManagementSystem/TicketService.Test/TicketServiceTest.cs
@@ -29,9 +29,8 @@ namespace TicketServiceTest
             TicketRepoWrapperMock.Setup(x => x.UpdateTicket(It.IsAny<Ticket>())).Callback<Ticket>(r => result = r);
 
             userServiceMock.Setup(x => x.GetUser(It.IsAny<string>())).Returns(new User());
-            priorityFactory.Setup(x => x.GetPriorityStrategy(Priority.High)).Returns(new PriorityStrategy());
-            priorityFactory.Setup(x => x.GetPriorityStrategy(Priority.Medium)).Returns(new PriorityStrategy());
-            priorityFactory.Setup(x => x.GetPriorityStrategy(Priority.Low)).Returns(new PriorityStrategy());
+
+            priorityFactory.Setup(x => x.GetPriorityStrategy(It.IsAny<Priority>())).Returns(new PriorityStrategy());
 
             _ticketService = new TicketService(userServiceMock.Object, TicketRepoWrapperMock.Object, calculatePriority.Object,
                 priorityFactory.Object);
@@ -73,6 +72,83 @@ namespace TicketServiceTest
             Assert.That(() => _ticketService.CreateTicket(title, It.IsAny<Priority>(), It.IsAny<string>(), description, It.IsAny<DateTime>(), It.IsAny<bool>()), Throws.TypeOf<InvalidTicketException>());
         }
 
+        [Test]
+        public void ShallCreateNormalTicket()
+        {
+            var x = new User() { FirstName = "A", LastName = "W", Username = "AW" };
+            userServiceMock.Setup(x => x.GetAccountManager()).Returns(x);
+
+            DateTime dateTime = DateTime.Now;
+            string desc = "I have an issue";
+            string title = "Help issue";
+            Priority prio = Priority.High;
+            calculatePriority.Setup(x => x.Calculate(It.IsAny<Priority>(), It.IsAny<DateTime>(), It.IsAny<string>())).Returns(prio);
+
+            var ticket = new Ticket()
+            {
+                AssignedUser = x,
+                Created = dateTime,
+                Description = desc,
+                Title = title,
+                Priority = prio
+            };
+
+            var resultTicket = _ticketService.CreateTicket(ticket.Title, ticket.Priority, ticket.AssignedUser.Username, ticket.Description, ticket.Created, false);
+
+            Assert.That(() => result?.Title == ticket.Title
+             && result?.Priority == ticket.Priority
+             && result?.Created == dateTime
+             && result?.Description == desc
+             && result.PriceDollars == ticket.PriceDollars
+            );
+            Assert.That(() => result?.PriceDollars == 0);
+            Assert.That(() => result?.AccountManager == null);
+        }
+
+        [Test]
+        public void ShallCreatePricedTicket()
+        {
+            var accountmanager = new User() { FirstName = "A", LastName = "W", Username = "AW" };
+            userServiceMock.Setup(x => x.GetAccountManager()).Returns(accountmanager);
+            var user = new User() { FirstName = "B", LastName = "Z", Username = "BZ" };
+            userServiceMock.Setup(x => x.GetUser(It.IsAny<string>())).Returns(user);
+
+            DateTime dateTime = DateTime.Now;
+            string desc = "I have an issue";
+            string title = "Help issue";
+            Priority prio = Priority.High;
+            calculatePriority.Setup(x => x.Calculate(It.IsAny<Priority>(), It.IsAny<DateTime>(), It.IsAny<string>())).Returns(prio);
+
+            var ticket = new Ticket()
+            {
+                PriceDollars = 50,
+                AccountManager = accountmanager,
+                AssignedUser = user,
+                Created = dateTime,
+                Description = desc,
+                Title = title,
+                Priority = prio
+            };
+
+            _ticketService.CreateTicket(ticket.Title, ticket.Priority, ticket.AssignedUser.Username, ticket.Description, ticket.Created, true);
+
+            Assert.That(() => result?.Title == ticket.Title 
+                        && result?.Priority == ticket.Priority
+                        && result?.Created == dateTime
+                        && result?.Description == desc
+                        && result.PriceDollars == ticket.PriceDollars
+                       );
+
+            Assert.That(() => result?.AccountManager.FirstName == accountmanager.FirstName
+                            && result?.AccountManager.LastName == accountmanager.LastName
+                            && result?.AccountManager.Username == accountmanager.Username
+                             );
+
+            Assert.That(() => result?.AssignedUser.FirstName == user.FirstName
+                && result?.AssignedUser.LastName == user.LastName
+                && result?.AssignedUser.Username == user.Username
+                 );
+        }
 
         [Test]
         public void ShallThrowExceptionOnNoTicket()
